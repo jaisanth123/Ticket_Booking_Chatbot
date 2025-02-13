1568b0ec88dc977b393dd4007583e5a14a4ff7e9
jaisanth123 <jaisanth2006@gmail.com>
2025-02-14 03:56:19 +0530

    razorpay error fixed

--- chatbot-frontend/src/PaymentMessageBubble.jsx
+++ chatbot-frontend/src/PaymentMessageBubble.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import razor from './assets/razor.jpeg'
 
 const PaymentMessageBubble = ({ order_id, setMessages }) => {
   const handlePayment = () => {
